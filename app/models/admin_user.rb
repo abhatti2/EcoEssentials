216774b94db120c079a31df5d6684f69d46afb8f
abhatti2 <abhatti2@rrc.ca>
2024-12-04 09:22:36 -0600

    added validations to 'admin_user.rb' model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,10 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
